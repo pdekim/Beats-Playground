5ca378dc7f0556c9ef4cdc1f24e224c8ad24c483
Peter Kim <31204165+pdekim@users.noreply.github.com>
2017-09-13 22:29:17 -0700

    buttons connected to sound but not working

--- Beats-Playground/ClassicViewController.swift
+++ Beats-Playground/ClassicViewController.swift
@@ -11,7 +11,12 @@ import AVFoundation
 
 class ClassicViewController: UIViewController {
     
-    var audioPlayer = AVAudioPlayer()
+    var bass1: AVAudioPlayer = AVAudioPlayer()
+    var bass2: AVAudioPlayer = AVAudioPlayer()
+    var bass3: AVAudioPlayer = AVAudioPlayer()
+    var snare1: AVAudioPlayer = AVAudioPlayer()
+    var snare2: AVAudioPlayer = AVAudioPlayer()
+    var snare3: AVAudioPlayer = AVAudioPlayer()
     
     
     @IBOutlet weak var ClassicBackgroundImage: UIImageView!
@@ -22,6 +27,83 @@ class ClassicViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         ClassicBackgroundImage.image = #imageLiteral(resourceName: "MainBackground")
+        
+        
+        let bass1File = Bundle.main.path(forResource: "Bass1", ofType: "mp3")
+        let bass2File = Bundle.main.path(forResource: "Bass2", ofType: "mp3")
+        let bass3File = Bundle.main.path(forResource: "Bass2", ofType: "mp3")
+        let snare1File = Bundle.main.path(forResource: "Snare1", ofType: "mp3")
+        let snare2File = Bundle.main.path(forResource: "Snare2", ofType: "mp3")
+        let snare3File = Bundle.main.path(forResource: "Snare2", ofType: "mp3")
+        
+        do {
+            
+            try bass1 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: bass1File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+        
+        do {
+            
+            try bass2 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: bass2File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+        
+        do {
+            
+            try bass3 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: bass3File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+
+        do {
+            
+            try snare1 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: snare1File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+
+        do {
+            
+            try snare2 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: snare2File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+
+        do {
+            
+            try snare3 = AVAudioPlayer(contentsOf: URL(fileURLWithPath: snare3File!))
+            
+        }
+        catch {
+            
+            print(error)
+            
+        }
+
+
+        
         
     }
     
@@ -35,37 +117,35 @@ class ClassicViewController: UIViewController {
     }
     
 
-    @IBAction func classicBeatPadBass(_ sender: UIButton) {
+    @IBAction func classicBeatPadBass(_ sender: BeatPadDesign) {
         if sender.tag == 1
         {
-
-        
+            bass1.play()
         }
         if sender.tag == 2
         {
-            
+            bass2.play()
         }
         if sender.tag == 3
         {
-            
+            bass3.play()
         }
     }
     
     
-    @IBAction func classicBeatPadSnare(_ sender: UIButton) {
+    @IBAction func classicBeatPadSnare(_ sender: BeatPadDesign) {
     
         if sender.tag == 4
         {
-    
-    
+            snare1.play()
         }
         if sender.tag == 5
         {
-    
+            snare2.play()
         }
         if sender.tag == 6
         {
-    
+            snare3.play()
         }
 
     }
